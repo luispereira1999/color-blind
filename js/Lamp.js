aad190aa2784e4178116efaadb38fb01f30e7fe7
Luís Pereira <67525203+luispereira1999@users.noreply.github.com>
2022-12-07 10:04:22 +0000

    Aplicar cor às lâmpadas

--- js/Lamp.js
+++ js/Lamp.js
@@ -12,8 +12,10 @@ class Lamp {
       this.y = y;
       this.width = width;
       this.height = height;
-      this.color = color;
       this.state = state;
+
+      this.opacity = this.getOpacity();
+      this.color = this.convertHexToRGBA(color, 0.85)
    }
 
    setImage(path) {
@@ -21,6 +23,36 @@ class Lamp {
       image.src = path;
       return image;
    }
+
+   getOpacity() {
+      let opacity = 0;
+
+      if (this.state == LAMP_STATE.ENABLE) {
+         opacity = 0.75;
+      } else {
+         opacity = 0.35;
+      }
+
+      return opacity;
+   }
+
+   convertHexToRGBA = (hexCode, opacity = 1) => {
+      let hex = hexCode.replace('#', '');
+
+      if (hex.length === 3) {
+         hex = `${hex[0]}${hex[0]}${hex[1]}${hex[1]}${hex[2]}${hex[2]}`;
+      }
+
+      const r = parseInt(hex.substring(0, 2), 16);
+      const g = parseInt(hex.substring(2, 4), 16);
+      const b = parseInt(hex.substring(4, 6), 16);
+
+      if (opacity > 1 && opacity <= 100) {
+         opacity = opacity / 100;
+      }
+
+      return `rgba(${r},${g},${b},${opacity})`;
+   };
 
    getBounds() {
       return {
@@ -47,7 +79,7 @@ class Lamp {
          false
       );
 
-      context.fillStyle = 'rgba(99, 255, 71, 0.45)';
+      context.fillStyle = this.color;
       context.fill();
    }
 }
