ca92d791e78245144dda48d245434ae1cbd99b31
Luís Pereira <67525203+luispereira1999@users.noreply.github.com>
2022-12-13 00:29:23 +0000

    Adicionar partículas quando ganha

--- js/Managers/GameManager.js
+++ js/Managers/GameManager.js
@@ -47,6 +47,7 @@ class GameManager {
             return;
          }
       }
+      // animationDoor = new AnimationManager("./assets/sprites/door.png", 166, 147, 4, 36, 1);
 
       // quando o jogado morre pelo inimigo
       if (this.currentLevel.player.state == PLAYER_STATE.DEAD) {
@@ -61,6 +62,163 @@ class GameManager {
          UIUtil.toggleScreen('winPopup', true);
          this.backToMenu("winPopup");
          this.goToNextLevel("winPopup");
+
+         tsParticles.load("tsparticles", {
+            fullScreen: {
+               enable: true
+            },
+            detectRetina: true,
+            background: {
+               opacity: 0
+            },
+            fpsLimit: 60,
+            emitters: {
+               direction: "top",
+               life: {
+                  count: 0,
+                  duration: 0.1,
+                  delay: 0.1
+               },
+               rate: {
+                  delay: 0.15,
+                  quantity: 1
+               },
+               size: {
+                  width: 100,
+                  height: 0
+               },
+               position: {
+                  y: 75,
+                  x: 50
+               }
+            },
+            particles: {
+               number: {
+                  value: 0
+               },
+               destroy: {
+                  mode: "split",
+                  split: {
+                     count: 1,
+                     factor: { value: 1 / 3 },
+                     rate: {
+                        value: 100
+                     },
+                     particles: {
+                        stroke: {
+                           color: {
+                              value: [
+                                 "#ffffff",
+                                 "#b22234",
+                                 "#b22234",
+                                 "#3c3bfe",
+                                 "#3c3bfe",
+                                 "#3c3bfe"
+                              ]
+                           },
+                           width: 1
+                        },
+                        number: {
+                           value: 0
+                        },
+                        collisions: {
+                           enable: false
+                        },
+                        opacity: {
+                           value: 1,
+                           animation: {
+                              enable: true,
+                              speed: 0.7,
+                              minimumValue: 0.1,
+                              sync: false,
+                              startValue: "max",
+                              destroy: "min"
+                           }
+                        },
+                        shape: {
+                           type: "circle"
+                        },
+                        size: {
+                           value: 1,
+                           animation: {
+                              enable: false
+                           }
+                        },
+                        life: {
+                           count: 1,
+                           duration: {
+                              value: {
+                                 min: 1,
+                                 max: 2
+                              }
+                           }
+                        },
+                        move: {
+                           enable: true,
+                           gravity: {
+                              enable: false
+                           },
+                           speed: 2,
+                           direction: "none",
+                           random: true,
+                           straight: false,
+                           outMode: "destroy"
+                        }
+                     }
+                  }
+               },
+               life: {
+                  count: 1
+               },
+               shape: {
+                  type: "line"
+               },
+               size: {
+                  value: 50,
+                  animation: {
+                     enable: true,
+                     sync: true,
+                     speed: 150,
+                     startValue: "max",
+                     destroy: "min"
+                  }
+               },
+               stroke: {
+                  color: {
+                     value: "#ffffff"
+                  },
+                  width: 1
+               },
+               rotate: {
+                  path: true
+               },
+               move: {
+                  enable: true,
+                  gravity: {
+                     acceleration: 15,
+                     enable: true,
+                     inverse: true,
+                     maxSpeed: 100
+                  },
+                  speed: { min: 10, max: 20 },
+                  outModes: {
+                     default: "destroy",
+                     top: "none"
+                  },
+                  trail: {
+                     fillColor: "#fff",
+                     enable: false,
+                     length: 10
+                  }
+               }
+            }
+         });
+
+         let particlesCanvas = document.querySelector(".tsparticles-canvas-el");
+         particlesCanvas.setAttribute("width", "896");
+         particlesCanvas.setAttribute("height", "576");
+         particlesCanvas.style.position = "absolute";
+
          return;
       }
 
@@ -116,7 +274,7 @@ class GameManager {
       let scale;
       let tileMap;
       let time;
-      let lives;
+      let music;
 
       switch (levelIndex) {
          case 1:
@@ -183,8 +341,8 @@ class GameManager {
             scale = 2.0;
             tileMap = new TileMapManager(tileSize, map, width, height, scale);
 
-            time = 15000;  // milissegundos
-            lives = 3;
+            time = 35000;  // milissegundos
+            music = 3;
             break;
          case 2:
             map = {
@@ -251,7 +409,7 @@ class GameManager {
             tileMap = new TileMapManager(tileSize, map, width, height, scale);
 
             time = 5000;  // milissegundos
-            lives = 3;
+            music = 3;
             break;
          case 3:
             map = {
@@ -318,7 +476,7 @@ class GameManager {
             tileMap = new TileMapManager(tileSize, map, width, height, scale);
 
             time = 5000;  // milissegundos
-            lives = 3;
+            music = 3;
             break;
          case 4:
             map = {
@@ -385,7 +543,7 @@ class GameManager {
             tileMap = new TileMapManager(tileSize, map, width, height, scale);
 
             time = 5000;  // milissegundos
-            lives = 3;
+            music = 3;
             break;
          case 5:
             map = {
@@ -451,7 +609,7 @@ class GameManager {
             tileMap = new TileMapManager(tileSize, map, width, height, scale);
 
             time = 5000;  // milissegundos
-            lives = 3;
+            music = 3;
             break;
       }
 
@@ -461,7 +619,7 @@ class GameManager {
          height: height,
          tileMap: tileMap,
          time: time,
-         lives: lives
+         music: music
       }
    }
 }
